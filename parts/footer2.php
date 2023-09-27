39a55032b80768b39bd0749de349b9dd031b7f08
HAMAD ALDOSSARY <hamaddossary@alMacBook-Pro-alkhas-bHAMAD.local>
2023-09-27 20:32:24 +0300

    first commit

---
+++ parts/footer2.php
@@ -0,0 +1,10 @@
+
+</div>
+
+<scriot src="./js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
+<script src="./js/script1.js"></script>
+<script src="./js/loader1.js"></script>
+
+</body>
+</html>
